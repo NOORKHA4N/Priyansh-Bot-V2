e8b2b808f94b0bd532d1a6f0ffa8edbe2b7292fe
NOORKHA4N <noorkhaan506@gmail.com>
2025-08-14 00:22:16 +0300

    Update 0_ARIF-BF-2.js

--- Priyansh/commands/0_ARIF-BF-2.js
+++ Priyansh/commands/0_ARIF-BF-2.js
@@ -23,7 +23,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'arr2.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/iaOiAXe.jpeg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.postimg.cc/WbbLhgHT/arr2.jpg", path); 
 }
 
 async function makeImage({ one, two }) {
