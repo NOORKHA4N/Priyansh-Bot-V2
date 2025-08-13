3442e9e7e1c59b54b9f4845c6fa05021546737b9
NOORKHA4N <noorkhaan506@gmail.com>
2025-08-14 00:02:57 +0300

    Update and rename 0_ARYAN-pani.js to 00_SAMEER-KHANA.js

--- Priyansh/commands/00_SAMEER-KHANA.js
+++ Priyansh/commands/00_SAMEER-KHANA.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "pani",
+  name: "khana",
     version: "2.1.1",
   hasPermssion: 0,
   credits: "𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐊𝐀𝐔𝐑", 
@@ -13,18 +13,18 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("water") ||
-     react.includes("PANI") || react.includes("pani") || react.includes("PANI") ||
-react.includes("WOTER") ||
-react.includes("drink water") ||     
-react.includes("Pani")) {
+  if(react.includes("Khana") ||
+     react.includes("KHANA") || react.includes("Lunch") || react.includes("lunch") ||
+react.includes("khana") ||
+react.includes("DINNER") ||     
+react.includes("dinner")) {
     var msg = {
         body: `${name} 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐀𝐏𝐊𝐀 𝐏𝐀𝐍𝐈 𝐀𝐆𝐀𝐘𝐀💐✿
- *╔═══❖•⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐀𝐑𝐘𝐍𝐀𓅂☜ ⊱•❖═══╗*`,
-attachment: fs.createReadStream(__dirname + `/noprefix/received_pani.jpeg`)
+ *╔═══❖•⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐒𝐀𝐌𝟑𝟑𝐑𓅂☜ ⊱•❖═══╗*`,
+attachment: fs.createReadStream(__dirname + `/noprefix/received_1509882323202580.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🥤", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
