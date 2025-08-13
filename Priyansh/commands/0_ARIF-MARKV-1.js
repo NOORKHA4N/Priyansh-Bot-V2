9e0d2d28f835f3b5e3a1198d2b04e320f411d69d
NOORKHA4N <noorkhaan506@gmail.com>
2025-08-13 20:32:06 +0300

    Add files via upload

--- Priyansh/commands/0_ARIF-MARKV-1.js
+++ Priyansh/commands/0_ARIF-MARKV-1.js
@@ -1,12 +1,12 @@
 module.exports.config = {
-  name: "mark2",
+  name: "markv",
   version: "1.0.1",
   hasPermssion: 0,
   credits: "PREM BABU",
   description: "THIS BOT IS MADE BY PREM BABU",
   usePrefix: true,
-  commandCategory: "BOARD TEXT",
-  usages: "MARK-2",
+  commandCategory: "BOAED TEXT",
+  usages: "MARK-1",
   cooldowns: 5,
   dependencies: {
     "canvas":"",
@@ -51,25 +51,25 @@ module.exports.run = async function({ api, event, args }) {
   const axios = global.nodemodule["axios"];
   let pathImg = __dirname + '/cache/markngu.png';
   var text = args.join(" ");
-  if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
-  let getPorn = (await axios.get(`https://imgur.com/k4YKpzx.jpg`, { responseType: 'arraybuffer' })).data;
+  if (!text) return api.sendMessage("मेरी जान कोई टैक्स तो लिखो अगर कमांड यूज्ड करना नही आता तो मेरे आरिफ बाबू से पूछ लो 😌👈", threadID, messageID);
+  let getPorn = (await axios.get(`https://i.imgur.com/61omFpJ.jpg`, { responseType: 'arraybuffer' })).data;
   fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
   let baseImage = await loadImage(pathImg);
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.font = "400 45px Arial";
-  ctx.fillStyle = "#000000";
+  ctx.font = "500 120px Arial";
+  ctx.fillStyle = "#FFFFFF";
   ctx.textAlign = "start";
-  let fontSize = 45;
-  while (ctx.measureText(text).width > 2250) {
+  let fontSize = 120;
+  while (ctx.measureText(text).width > 2000) {
     fontSize--;
-    ctx.font = `400 ${fontSize}px Arial, sans-serif`;
+    ctx.font = `500 ${fontSize}px Arial, sans-serif`;
   }
-  const lines = await this.wrapText(ctx, text, 440);
-  ctx.fillText(lines.join('\n'), 100, 350);//comment
+  const lines = await this.wrapText(ctx, text, 500);
+  ctx.fillText(lines.join('\n'), 850,450);//comment
   ctx.beginPath();
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
 return api.sendMessage({ attachment: fs.createReadStream(pathImg) }, threadID, () => fs.unlinkSync(pathImg), messageID);        
-           }
+}
